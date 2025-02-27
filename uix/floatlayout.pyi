6a212dacab88dd393db8736d42d7c2d56037a684
Scarlett Samantha Verheul <scarlett.verheul@gmail.com>
2025-02-27 17:37:35 +0100

    ✨ Add constructor to FloatLayout with size_hint, width, height, and pos_hint parameters
    Signed-off-by: Scarlett Samantha Verheul <scarlett.verheul@gmail.com>

--- uix/floatlayout.pyi
+++ uix/floatlayout.pyi
@@ -1,4 +1,12 @@
+from typing import Dict, Tuple
 from kivy.uix.layout import Layout
 
 class FloatLayout(Layout):
-    pass
+    def __init__(
+        self,
+        size_hint: Tuple[float | None, float | None],
+        width: int,
+        height: int,
+        pos_hint: Dict[str, float],
+        **kwargs,
+    ): ...
