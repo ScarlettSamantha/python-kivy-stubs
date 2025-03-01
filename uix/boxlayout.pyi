650cc49857bcf21e9fcc7072cd186b11479a0ca3
Scarlett Samantha Verheul <scarlett.verheul@gmail.com>
2025-03-01 17:12:02 +0100

    Added some small fixes to boxlayout and widget init
    Signed-off-by: Scarlett Samantha Verheul <scarlett.verheul@gmail.com>

--- uix/boxlayout.pyi
+++ uix/boxlayout.pyi
@@ -1,10 +1,12 @@
 from typing import Dict, Optional, Tuple
-
+from typing import Union
 from kivy.uix.widget import Widget
 from kivy.uix.label import Label
 from kivy.uix.layout import Layout
 from kivy.properties import NumericProperty, ReferenceListProperty, StringProperty
 from kivy.graphics.instructions import Canvas
+
+numeric = Union[int, float, complex]
 
 class BoxLayout(Layout):
     minimum_width: NumericProperty
@@ -18,8 +20,12 @@ class BoxLayout(Layout):
         self,
         orientation: Optional[str] = "horizontal",
         spacing: Optional[int] = 10,
-        padding: Optional[Tuple[int, int]] = None,
-        size_hint: Optional[Tuple[float, float]] = None,
+        height: Optional[numeric] = None,
+        width: Optional[numeric] = None,
+        padding: Optional[
+            Tuple[numeric, numeric] | Tuple[numeric, numeric, numeric, numeric]
+        ] = None,
+        size_hint: Optional[Tuple[float, float | None]] = None,
         pos_hint: Optional[Dict[str, float | int]] = None,
     ) -> None: ...
     def add_widget(self, widget: Layout | Widget | Label, *args, **kwargs) -> None: ...
