150609662f8c14ba6c54e2a0e5084d2c3928d2f3
Scarlett Samantha Verheul <scarlett.verheul@gmail.com>
2025-07-22 12:17:47 +0200

    🏷️ Add type annotations for spacing and padding properties in GridLayout class
    Signed-off-by: Scarlett Samantha Verheul <scarlett.verheul@gmail.com>

--- uix/gridlayout.pyi
+++ uix/gridlayout.pyi
@@ -1,8 +1,11 @@
-from typing import Any
+from typing import Any, List, Tuple
 
 from kivy.uix.layout import Layout
 
 
 class GridLayout(Layout):
+    
+    spacing: Tuple[int, ...] | List[int] | int 
+    padding: Tuple[int, ...] | List[int] | int
     
     def do_layout(self, *largs: Any) -> None: ...
